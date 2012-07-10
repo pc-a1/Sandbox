c90475da4ddead5f3960ef1c87761b1a60b2ffff
Paul Cothenet <paul.cothenet@agilone.com>
2012-07-09 21:49:41 -0700

    First compilation

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Paul
+ * Date: 7/9/12
+ * Time: 9:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
